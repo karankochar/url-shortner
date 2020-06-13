e101698aab867aaa27cc85467a705475f104b2fe
Karandeep <karankochar302gmail.com>
2020-06-13 18:54:31 +0530

    Front-end added

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const cors = require('cors');
 
 const app = express();
 connectDB();
 
 app.use(express.json({ extended:false }));
+app.use(cors());    
 
 app.use('/', require('./routes/index'));
 app.use('/api/url', require('./routes/url'));
